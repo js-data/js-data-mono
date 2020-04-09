8efff23f23cddff6c77d96b54b07ff26e5602160
Luis Vargas <luisvargastijerino@gmail.com>
2020-04-09 15:36:35 -0600

    fix libraries dependencies and build

--- apps/js-data-demo/src/app/app.module.ts
+++ apps/js-data-demo/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { JsDataAngularxModule } from '@js-data/js-data-angularx';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, JsDataAngularxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
